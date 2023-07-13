d869dede1fb0cb49bef59bf40d25833c3c08dda0
lordprotein <gamee.logi@gmail.com>
2023-07-13 14:53:37 +0600

    progress

--- build/lintstagedrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '*.{ts,tsx,js,jsx}': [() => 'yarn lint:ts', 'yarn lint:eslint'],
-  '*.{js,jsx,ts,tsx,html,css}': ['yarn lint:prettier'],
-  '*.{css,scss,js,ts,jsx,tsx}': ['yarn lint:style']
-}
